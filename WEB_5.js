2f57c2c666b01f906e952bc62b2fe6cae7a800eb
AfaqAhsan <88290542+AfaqAhsan@users.noreply.github.com>
2021-10-05 21:05:33 +0500

    calling smart contract functions

---
+++ WEB_5.js
@@ -0,0 +1,103 @@
+const Web3 = require('web3');
+const rpcURL = "https://ropsten.infura.io/v3/d2e382f4eddc45c19db782cce7a59b2a";
+const web3 = new Web3(rpcURL);
+const contractAddress = "0x65785e8862d5ad2de30a2ee522f310c39ae2dc72";
+const contractABI = [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "event_name",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "event_name2",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "age",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getAge",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "x",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "y",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+const contract = new web3.eth.Contract(contractABI, contractAddress);
+const eventFunction = async () => {
+    try {
+        let getAllEvent = await contract.getPastEvents("AllEvents", {
+            fromBlock: 0,
+            toBlock: "latest"
+        });
+        console.log("getAllEvent :", getAllEvent);
+
+    }catch(err) {
+        console.log(err);
+    }
+}
+eventFunction();
